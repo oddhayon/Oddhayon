517cfe4eeb12bdf272dcdfcf24739e09ee80a573
Oddhayon <xpzunayed.contact@gmail.com>
2026-08-14 18:37:56 +0600

    Add Android APK build workflow

---
+++ android/app/src/main/java/com/oddhayon/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.oddhayon.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oddhayon.app',
+  appName: 'Oddhayon',
+  webDir: 'dist'
+};
+
+export default config;
